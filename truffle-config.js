f7348a2a2c970d9ad21a05d59d9e84cfc4e4a5bb
Artem Subbotin <subbotin.artem@gmail.com>
2021-08-30 22:52:49 +0300

    custom `transferRule` for src20

--- truffle-config.js
+++ truffle-config.js
@@ -52,7 +52,7 @@ module.exports = {
         host: "127.0.0.1",     // Localhost (default: none)
         port: 8545,            // Standard Ethereum port (default: none)
         network_id: "*",       // Any network (default: none)
-        gas: 11000000,
+        gas: 18000000,
         //confirmations: 2,    
         timeoutBlocks: 200,  
     },
@@ -133,12 +133,12 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.3",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.4",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 600
+          runs: 200
         },
       //  evmVersion: "byzantium"
       
@@ -146,7 +146,7 @@ module.exports = {
     },
   },
   
-  plugins: ['truffle-plugin-verify'],
+  plugins: ['truffle-plugin-verify', 'truffle-contract-size'],
   
   api_keys: {
     // etherscan: process.env.etherscan_api_key,
